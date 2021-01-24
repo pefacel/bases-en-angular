537416b5013b7543b5edc6e47b33cd2bdbbf57e1
pefacel <pefacel@gmail.com>
2021-01-23 22:49:42 -0300

    Botones de +1 y -1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   titulo: string = 'Hola Mundo!';
   numero: number = 10;
+  base  : number = 5;
+
+  acumular(valor: number) {
+    this.base += this.base;
+
+
+
+  }
 
 }
